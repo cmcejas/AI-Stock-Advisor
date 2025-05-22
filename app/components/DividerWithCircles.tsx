1d4099e09c925162de07c6043bc1a5e90c137703
cmcejas <carlosmcejas@gmail.com>
2025-05-22 19:38:57 -0400

    finalized highlight color

--- app/components/DividerWithCircles.tsx
+++ app/components/DividerWithCircles.tsx
@@ -1,6 +1,6 @@
 const DividerWithCircles = () => {
   return (
-    <div className="relative my-8 flex items-center justify-center px-10 py-10 opacity-25">
+    <div className="relative my-8 flex items-center justify-center px-10 pb-2 opacity-25">
       {/* Line */}
       <div className="h-[3px] w-full bg-white shadow-2xl" />
 
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <header className="rounded-4xl bg-[#ffffe7] p-4 text-white">
-      <h1 className="text-center text-3xl font-bold text-[#DCBB57]">
-        Gold Leaf Analytics
+      <h1 className="text-center text-3xl font-extrabold text-[#DCBB57]">
+        GOLD LEAF ANALYTICS
       </h1>
     </header>
   )
